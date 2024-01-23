f10b82a197d40500a8a2d96764674d8cccea2b0a
SPZRH\kishore.raj <kishore.raj@synpulse.com>
2024-01-23 12:23:47 +0800

    --Update SVG icons

--- src/components/common/infinte_table/actionItem.jsx
+++ src/components/common/infinte_table/actionItem.jsx
@@ -1,27 +1,21 @@
-
-import EditIcon from "../../../assets/editIcon"
-import DeleteIcon from "../../../assets/deleteIcon"
+import Image from "next/image";
+import EditIcon from "../../../../public/static/images/editIcon.svg";
+import DeleteIcon from "../../../../public/static/images/deleteIcon.svg";
 
 const ActionItem = ({ actionType, onClick }) => {
-    function renderIcon(actionType) {
-        switch(actionType) {
-          case 'Edit':
-            return <EditIcon/>;
-            case 'Delete':
-                return <DeleteIcon/>;
+  function renderIcon(actionType) {
+    switch (actionType) {
+      case "Edit":
+        return <Image priority src={EditIcon} />;
+      case "Delete":
+        return <Image priority src={DeleteIcon} />;
 
-          default:
-            return <></>;
-        }
-      }
+      default:
+        return <></>;
+    }
+  }
 
-    return (
-        <button 
-            onClick={onClick} 
-        >
-            {renderIcon(actionType)}
-        </button>
-    );
-  };
-  
-  export default ActionItem;
+  return <button onClick={onClick}>{renderIcon(actionType)}</button>;
+};
+
+export default ActionItem;
