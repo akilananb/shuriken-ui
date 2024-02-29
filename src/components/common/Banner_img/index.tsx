df972e90b84611016150cc1d61c0b465c945868e
Zainul Abedin Marchawala <zainulabedin.marchawala@synpulse.com>
2024-02-29 12:59:16 +0530

    features(banner_issue)Design the banner, showed on landing page and sort out issue of Banner Image (#32)
    Co-authored-by: zain-zithas <zainm@zithas.com>

---
+++ src/components/common/Banner_img/index.tsx
@@ -0,0 +1,12 @@
+interface Props {
+    src: string;
+    className?: string;
+  }
+
+  const Index: React.FC<Props> = ({ src, className }) => {
+    return (
+      <img src={src} className={className} alt='Banner Img' />
+    );
+  };
+
+  export default Index;
--- src/components/layout/landing/index.tsx
+++ src/components/layout/landing/index.tsx
@@ -4,6 +4,8 @@ import LTVSearchInput from "@/components/common/ltv_search_input";
 import { Input, Switch } from "antd";
 import Link from "next/link";
 import SearchView from "./searchView";
+import BannerImg from '@/components/common/Banner_img'
+import { BASE_NAME } from "@/config/appConfig";
 
 export async function getLadingPageData() {
   try {
@@ -29,7 +31,12 @@ export async function getLadingPageData() {
 export default async function Home() {
   const { announcementData } = await getLadingPageData();
   return (
-    <div className="flex flex-row bg-white h-full justify-center items-center">
+    <div className="flex flex-col h-full">
+      <div className="h-[30%]">
+        <BannerImg src={`${BASE_NAME}/static/images/nomura_banner.jpeg`} className='h-full object-cover w-full opacity-30' />
+      </div>
+
+    <div className="flex flex-row bg-white justify-center items-center h-[70%] min-h-96 pt-10">
       <div className="flex flex-col items-center justify-center gap-10 w-[746px] relative -top-16">
         <div className="text-justify leading-normal text-2xl font-bold ">
           LTV Search
@@ -40,6 +47,7 @@ export default async function Home() {
         />
         <SearchView />
       </div>
+    </div>
     </div>
   );
 }
