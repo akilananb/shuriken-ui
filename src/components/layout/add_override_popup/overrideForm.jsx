464b1369f4fab8a46cd05ea55a75546772569c82
Shreyas Kulkarni <shreyas.kulkarni@synpulse.com>
2024-03-06 09:23:40 +0530

    changes for addoverride (#34)
    * changes for addoverride
    
    * changes for add override search value

--- src/components/layout/add_override_popup/overrideForm.jsx
+++ src/components/layout/add_override_popup/overrideForm.jsx
@@ -1,71 +1,78 @@
-import { Field } from "formik";
+import { Field, useField } from "formik";
 import ErrorField from "@/components/common/ErrorField";
+import LTVSearchInput from "../../common/ltv_search_input";
 
-const OverridesForm = ({ errors, touched }) => (
-  <>
-    <ErrorField
-      errorMsg={
-        errors.instrumentId && touched.instrumentId && errors["instrumentId"]
-      }
-    >
-      <Field
-        className="override-input"
-        type="text"
-        placeholder="Search"
-        name="instrumentId"
-      />
-    </ErrorField>
-    <ErrorField
-      errorMsg={
-        errors.ltvOverrideValue &&
-        touched.ltvOverrideValue &&
-        errors["ltvOverrideValue"]
-      }
-    >
-      <Field
-        type="number"
-        placeholder="LTV at IM"
-        className="override-input"
-        name="ltvOverrideValue"
-      />
-    </ErrorField>
-    <div className="flex flex-row gap-2 ">
+const OverridesForm = ({ errors, touched }) => {
+  const [instrumentField, instrumentMeta, instrumentHelpers] =
+    useField("instrumentId");
+  return (
+    <>
       <ErrorField
-        errorMsg={errors.startDate && touched.startDate && errors["startDate"]}
+        errorMsg={
+          errors.instrumentId && touched.instrumentId && errors["instrumentId"]
+        }
       >
-        <Field
-          type="date"
-          placeholder="Valid From"
-          className="override-input"
-          name="startDate"
+        <LTVSearchInput
+          name="instrumentId"
+          value={instrumentField.value}
+          onSelectedItem={(value) => instrumentHelpers.setValue(value)}
+          error={instrumentMeta.touched && instrumentMeta.error}
         />
       </ErrorField>
       <ErrorField
-        errorMsg={errors.endDate && touched.endDate && errors["endDate"]}
+        errorMsg={
+          errors.ltvOverrideValue &&
+          touched.ltvOverrideValue &&
+          errors["ltvOverrideValue"]
+        }
       >
         <Field
-          type="date"
-          placeholder="Valid Till"
+          type="number"
+          placeholder="LTV at IM"
           className="override-input"
-          name="endDate"
+          name="ltvOverrideValue"
         />
       </ErrorField>
-    </div>
-    <ErrorField
-      errorMsg={
-        errors.ltvOverrideNote &&
-        touched.ltvOverrideNote &&
-        errors["ltvOverrideNote"]
-      }
-    >
-      <Field
-        type="text"
-        placeholder="Why are you adding this override?"
-        className="override-input"
-        name="ltvOverrideNote"
-      />
-    </ErrorField>
-  </>
-);
+      <div className="flex flex-row gap-2 ">
+        <ErrorField
+          errorMsg={
+            errors.startDate && touched.startDate && errors["startDate"]
+          }
+        >
+          <Field
+            type="date"
+            placeholder="Valid From"
+            className="override-input"
+            name="startDate"
+          />
+        </ErrorField>
+        <ErrorField
+          errorMsg={errors.endDate && touched.endDate && errors["endDate"]}
+        >
+          <Field
+            type="date"
+            placeholder="Valid Till"
+            className="override-input"
+            name="endDate"
+          />
+        </ErrorField>
+      </div>
+      <ErrorField
+        errorMsg={
+          errors.ltvOverrideNote &&
+          touched.ltvOverrideNote &&
+          errors["ltvOverrideNote"]
+        }
+      >
+        <Field
+          type="text"
+          placeholder="Why are you adding this override?"
+          className="override-input"
+          name="ltvOverrideNote"
+        />
+      </ErrorField>
+    </>
+  );
+};
 
 export default OverridesForm;
